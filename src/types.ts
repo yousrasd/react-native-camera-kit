738e06a8b4a3639648d59d0d66c25b3e55e1e9f8
Yousra Saidani <ysaidani@pbsc.com>
2023-02-01 16:12:02 -0500

    chore: expose a method to set the torch mode

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,6 @@
 export type CameraApi = {
   capture: () => Promise<{ uri: string }>,
+  setTorchMode: (mode: String) => void;
   requestDeviceCameraAuthorization: () => Promise<boolean>,
   checkDeviceCameraAuthorizationStatus: () => Promise<boolean>,
 };
